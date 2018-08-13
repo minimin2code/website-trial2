21cbcd1982d0f07c9645d68f98c4a9279923ceaa
minimin2code <41496791+minimin2code@users.noreply.github.com>
2018-08-13 23:33:00 +0800

    Add files via upload

---
+++ public/src/TodoList.js
@@ -0,0 +1,70 @@
+import React from "react"; 
+import "./TodoList.css";
+import TodoItems from "./TodoItems";
+
+class TodoList extends React.Component { 
+  constructor(props) {    
+    super(props);     
+
+    this.state = {    
+      items: []  
+    };
+
+    this.addItem = this.addItem.bind(this); 
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== "") {    
+      itemArray.unshift(
+        {      
+          text: this._inputElement.value,      
+          key: Date.now()    
+        }
+      );
+
+      this.setState({      
+        items: itemArray    
+      });   
+
+      this._inputElement.value = "";  
+    }   
+    console.log(itemArray);   
+    e.preventDefault();
+    
+  }
+
+  deleteItem(key) {  
+    var filteredItems = this.state.items.filter(function (item) {    
+      return (item.key !== key);  
+    }); 
+
+    this.setState({    
+      items: filteredItems  
+    });
+  }
+
+  render() {    
+    return (      
+      <div className="todoListMain">        
+        <div className="header">          
+          <form onSubmit={this.addItem}> 
+               <h1 className="app-header"> Simple but Awesome :) </h1>      
+            <input ref={(a) => this._inputElement = a} 
+              placeholder="enter your task">
+            </input>       
+            <button type="submit">Submit</button>
+            </form> 
+                  
+        </div>
+
+        <TodoItems entries={this.state.items} 
+                  delete={this.deleteItem}/>     
+      </div>    
+    );  
+  }
+} 
+
+export default TodoList;
